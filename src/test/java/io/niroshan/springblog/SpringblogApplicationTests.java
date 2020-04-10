506f57e33231e145903daeaf4ce4a80896a2a81f
Niroshan07 <Niroshan07@bitbucket.com>
2020-04-10 23:46:19 +0530

    setup done

---
+++ src/test/java/io/niroshan/springblog/SpringblogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.niroshan.springblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
