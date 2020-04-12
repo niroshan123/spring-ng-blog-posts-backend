ad6fc8539690a398f47162e0ba7b7dce0576765f
Niroshan07 <Niroshan07@bitbucket.com>
2020-04-12 15:07:05 +0530

    security added
    Signed-off-by: Niroshan07 <Niroshan07@bitbucket.com>

--- src/main/java/io/niroshan/springblog/contoller/AuthController.java
+++ src/main/java/io/niroshan/springblog/contoller/AuthController.java
@@ -1,5 +1,6 @@
 package io.niroshan.springblog.contoller;
 
+import io.niroshan.springblog.dto.LoginRequest;
 import io.niroshan.springblog.dto.RegisterRequest;
 import io.niroshan.springblog.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,4 +25,11 @@ public class AuthController {
     return new ResponseEntity(HttpStatus.OK);
 
     }
+
+    @PostMapping("/login")
+    public void login(@RequestBody LoginRequest loginRequest){
+
+
+    }
+
 }
--- src/main/java/io/niroshan/springblog/dto/LoginRequest.java
+++ src/main/java/io/niroshan/springblog/dto/LoginRequest.java
@@ -1,4 +1,7 @@
 package io.niroshan.springblog.dto;
 
 public class LoginRequest {
+    private String username;
+    private String password;
+
 }
